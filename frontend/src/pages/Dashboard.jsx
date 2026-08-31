44c0a19bc698dac6cae0c8075788706dcdbb0d3c
Ashan Salinda <ashansalinda5@gmail.com>
2026-09-01 01:46:51 +0530

    Dashboard hidden

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
+import { Navigate } from "react-router-dom";
+
 export default function Dashboard() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-      <p>Insights and analytics will go here.</p>
-    </div>
+    <Navigate to="/bookings" replace={true} />
+    // <div className="p-8">
+    //   <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+    //   <p>Insights and analytics will go here.</p>
+    // </div>
   );
 }
